e8baa287a78dc0ea6e9a8fac64336da1838cf4fe
YuNandar118 <yunandar15502@gmail.com>
2020-12-15 18:09:47 -0800

    added route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+if(App::environment('production')){
+    URL::forceScheme('https');
+}
 Route::get('/', function () {
     return view('welcome');
 });
